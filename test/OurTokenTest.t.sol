cb52ba8d5032691360240955be855c13e24a73f2
bigblock <bigblock@localhost.localdomain>
2024-11-04 18:47:23 -0500

    Code Formatting

--- test/OurTokenTest.t.sol
+++ test/OurTokenTest.t.sol
@@ -6,27 +6,25 @@ import {Test} from "forge-std/Test.sol";
 import {OurToken} from "../src/OurToken.sol";
 import {DeployOurToken} from "../script/DeployOurToken.s.sol";
 
-contract OurTokenTest is Test{
+contract OurTokenTest is Test {
     OurToken public ourToken;
     DeployOurToken public deployer;
 
     address bob = makeAddr("bob");
     address alice = makeAddr("alice");
 
-    uint256 public constant STARTING_BALANCE= 100 ether;
+    uint256 public constant STARTING_BALANCE = 100 ether;
 
     function setUp() public {
-
         deployer = new DeployOurToken();
         ourToken = deployer.run();
-        
+
         vm.prank(msg.sender);
         ourToken.transfer(bob, STARTING_BALANCE);
-
     }
 
     function testBobBalance() public {
-        assertEq(STARTING_BALANCE,ourToken.balanceOf(bob));
+        assertEq(STARTING_BALANCE, ourToken.balanceOf(bob));
     }
 
     function testAllowancesWorks() public {
@@ -38,9 +36,9 @@ contract OurTokenTest is Test{
         uint256 transferAmount = 500;
 
         vm.prank(alice);
-        ourToken.transferFrom(bob,alice,transferAmount);
+        ourToken.transferFrom(bob, alice, transferAmount);
 
-        assertEq(ourToken.balanceOf(alice),transferAmount);
-        assertEq(ourToken.balanceOf(bob), STARTING_BALANCE-transferAmount);
+        assertEq(ourToken.balanceOf(alice), transferAmount);
+        assertEq(ourToken.balanceOf(bob), STARTING_BALANCE - transferAmount);
     }
 }
